9127c56727b259493b0686318be04b0231cbc832
David Trame <king_bucket@hotmail.com>
2017-01-20 21:57:21 -0500

    Drew a canvas and got zoom working with the scale function!

--- tkinter_demo.py
+++ tkinter_demo.py
@@ -8,6 +8,8 @@ class Window(tk.Frame):
         tk.Frame.__init__(self, master)
 
         self.master = master
+        self.scale_factor = 1
+        self.canvas = tk.Canvas(self)
 
         self.init_window()
 
@@ -17,10 +19,28 @@ class Window(tk.Frame):
         #pack this into our frame
         self.pack(fill=tk.BOTH, expand=1)
 
-        #create a button instance
-        #quitButton = tk.Button(self, text='Quit', command=self.client_exit)
+        #canvas = tk.Canvas(self)
+        self.canvas.create_line(300, 35, 300, 200)
+        self.canvas.create_line(15, 25, 200, 25, dash=(4, 2))
+        self.canvas.create_line(55, 85, 155, 85, 105, 180, 55, 85)
+        self.canvas.create_rectangle(30, 10, 120, 80, outline="#fb0", fill="#05f")
+        self.canvas.pack(fill=tk.BOTH, expand=1)
 
-        #quitButton.place(x=0, y=0)
+        points = [150, 100, 200, 120, 240, 180, 210,
+                  200, 150, 150, 100, 200]
+        self.canvas.create_polygon(points, outline='red',
+                              fill='green', width=2)
+
+
+
+        self.master.bind("<MouseWheel>", self.zoom)
+
+
+
+        #create a button instance
+        quitButton = tk.Button(self, text='Quit', command=self.client_exit)
+
+        quitButton.place(x=0, y=0)
 
         #this is a menu of the main window
         myMenu = tk.Menu(self.master)
@@ -40,6 +60,16 @@ class Window(tk.Frame):
         myEdit.add_command(label='Show Text', command=self.showTxt)
         myMenu.add_cascade(label='Edit', menu=myEdit)
 
+    def zoom(self, event):
+        if event.delta > 0:
+            self.scale_factor = 1.1
+            print('Zoom in!  New scale factor: ' + str(self.scale_factor))
+        else:
+            self.scale_factor = 0.9
+            print('Zoom out!  New scale factor: ' + str(self.scale_factor))
+
+        self.canvas.scale(tk.ALL, 0, 0, self.scale_factor, self.scale_factor)
+
     def showImg(self):
         load = Image.open('C:\\Code\\David_Python_Testing\\apple.png')
         render = ImageTk.PhotoImage(load)
@@ -56,7 +86,7 @@ class Window(tk.Frame):
         exit()
 
 root = tk.Tk()
-root.geometry("400x300")
+root.geometry("800x600")
 
 app = Window(root)
 
